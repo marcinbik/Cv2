f30211751054dcd502aed8656e9ee0e0bdfda83c
Marcin Bikiewicz <marcinbik55@gmail.com>
2022-12-01 19:26:49 +0100

    fix card

--- front/components/Card.js
+++ front/components/Card.js
@@ -1,27 +1,23 @@
 import { faRocket } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
+import { root } from 'postcss';
 
 
 
-function pra(text) {
-    
-    
-    text.forEach(element => {
-        {
-            return(
-                <div className='p-5'>
-                    <p>
-                        <FontAwesomeIcon className='pr-2' icon={faRocket}/>
-                        {element}
-                        
-                    </p>
-                </div>
-        ) }
-    }
-    );
-}
 export default function Card({img,title ,  text }){
+    
+        
+        const listItems = text.map((element) => 
+                    <div className='p-3'>
+                        <p>
+                            <FontAwesomeIcon className='pr-2' icon={faRocket}/>
+                            {element}
+                        
+                        </p>
+                    </div>
+        );
+    
 
     return(
         <div className="p-5 rounded-lg">
@@ -35,8 +31,8 @@ export default function Card({img,title ,  text }){
             <div>
             <h2 className="text-center p-5 text-2xl">{title}</h2>
             </div>
-          
-          {pra(text)}
+          {listItems}
+          <paragraf text={text}/>
             <Link href="/services" passHref >
             <button className="bg-[#569DA7] w-full h-[6vh] mt-10 rounded-lg  hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ">
                 Sprawdź Ofertę
--- front/pages/services.js
+++ front/pages/services.js
@@ -2,16 +2,15 @@ import Navbar from "../components/Navbar"
 import Contact from "../components/Contact"
 import Title from "../components/Title"
 import Card from "../components/Card"
-import { faChampagneGlasses } from "@fortawesome/free-solid-svg-icons"
 
 
 export default function Services()
 {
     const helper = []
-         helper.push('Sześć miesięcy darmowego wsparcia')
+        helper.push('Sześć miesięcy darmowego wsparcia')
          helper.push('Szybka realizacja')
          helper.push('Dopasowanie do potrzeb klienta ')
-    console.log(helper)
+    
     return(
         <>
         <Navbar/>
