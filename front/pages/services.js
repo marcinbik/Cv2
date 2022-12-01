b7574ffa7408e24da75ad3cd0b4c0cc976f5ac0d
Marcin Bikiewicz <marcinbik55@gmail.com>
2022-12-01 15:35:28 +0100

    Card War

--- front/pages/services.js
+++ front/pages/services.js
@@ -2,13 +2,21 @@ import Navbar from "../components/Navbar"
 import Contact from "../components/Contact"
 import Title from "../components/Title"
 import Card from "../components/Card"
+import { faChampagneGlasses } from "@fortawesome/free-solid-svg-icons"
+
+
 export default function Services()
 {
+    const helper = []
+         helper.push('Sześć miesięcy darmowego wsparcia')
+         helper.push('Szybka realizacja')
+         helper.push('Dopasowanie do potrzeb klienta ')
+    console.log(helper)
     return(
         <>
         <Navbar/>
         <Title title={"Dopasuj technologie do twoich potrzeb "}/>
-        <Card/>
+        <Card img={'/wordpress.jpg'} title={"Nowoczesne Aplikacje" } text={helper} />
 
 
 
