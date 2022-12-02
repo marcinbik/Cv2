8805f7f1767285f337b811619e7ecb62825116f2
Marcin Bikiewicz <marcinbik55@gmail.com>
2022-12-02 13:59:40 +0100

    add cto list serwices page

--- front/components/Card.js
+++ front/components/Card.js
@@ -25,19 +25,19 @@ export default function Card({img,title ,  text }){
         <div className=" container bg-[#DFDFDF] rounded-lg ">
             <div className="grid w-full h-[20vh]  object-contain overflow-hidden " >
 
-            <img src={img} alt='cardimage' className=" rounded-lg  overflow-hidden"  />
+                <img src={img} alt={title} className=" rounded-lg  overflow-hidden"  />
             </div>
             
             <div>
-            <h2 className="text-center p-5 text-2xl">{title}</h2>
+                <h2 className="text-center p-5 text-2xl">{title}</h2>
             </div>
-          {listItems}
-          <paragraf text={text}/>
+            {listItems}
+            <paragraf text={text}/>
             <Link href="/services" passHref >
-            <button className="bg-[#569DA7] w-full h-[6vh] mt-10 rounded-lg  hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ">
-                Sprawdź Ofertę
-            </button>
-    </Link>
+                <button className="bg-[#569DA7] w-full h-[6vh] mt-10 rounded-lg  hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ">
+                    Sprawdź Ofertę
+                </button>
+            </Link>
             
         
         </div>
---
+++ front/components/ListCto.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointRight} from '@fortawesome/free-solid-svg-icons'
+
+export default function ListCto()
+{ 
+
+    return(
+
+<div className="flex-1 px-5  py-10 text-lg justify-center">
+  
+
+  <p className='py-2'>
+    <FontAwesomeIcon icon={faHandPointRight} className="pr-2 text-xl" />
+    Dobierzemy odpowiednie rozwiązanie</p>
+  
+  <p className='py-2'><FontAwesomeIcon icon={faHandPointRight} className="pr-2 text-xl" />
+  Od prostych stron po duże aplikacje </p>
+  <p className='py-2'><FontAwesomeIcon icon={faHandPointRight} className="pr-2 text-xl" />
+  Udzielamy darmowego wsparcia</p>
+  </div>
+    )
+
+
+
+}
